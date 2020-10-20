8c4e6ab50f788d44a4a5540f7a4c9021ffa2d62e
Neale Johnston <neale@team-johnston.com>
2020-10-20 14:55:59 +0100

    One mahoosive commit as I forgot :(

---
+++ src/bus.py
@@ -0,0 +1,25 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengers = []
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+
+    def empty(self):
+        # takes away the need to remove one at a time, 'clear' empties a list
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop):
+        for passenger in bus_stop.queue:
+            self.passengers.append(passenger)
--- tests/bus_test.py
+++ tests/bus_test.py
@@ -1,56 +1,53 @@
 import unittest
 from src.bus import Bus
+
 from src.bus_stop import BusStop
 from src.person import Person
+
 
 class TestBus(unittest.TestCase):
     def setUp(self):
         self.bus = Bus(22, "Ocean Terminal")
 
-    @unittest.skip("Delete this line to run the test")
     def test_has_route_number(self):
         self.assertEqual(22, self.bus.route_number)
 
-
-    @unittest.skip("Delete this line to run the test")
     def test_has_destination(self):
         self.assertEqual("Ocean Terminal", self.bus.destination)
 
-
-    @unittest.skip("Delete this line to run the test")
     def test_can_drive(self):
         self.assertEqual("Brum brum", self.bus.drive())
 
-    @unittest.skip("Delete this line to run the test")
     def test_starts_with_no_passengers(self):
         self.assertEqual(0, self.bus.passenger_count())
 
-    @unittest.skip("Delete this line to run the test")
     def test_can_pick_up_passenger(self):
         person = Person("Guido van Rossum", 64)
         self.bus.pick_up(person)
         self.assertEqual(1, self.bus.passenger_count())
 
-    @unittest.skip("Delete this line to run the test")
     def test_can_drop_off_passenger(self):
         person = Person("Guido van Rossum", 64)
         self.bus.pick_up(person)
         self.bus.drop_off(person)
         self.assertEqual(0, self.bus.passenger_count())
 
-    @unittest.skip("Delete this line to run the test")
     def test_can_empty_bus(self):
         person = Person("Guido van Rossum", 64)
         self.bus.pick_up(person)
         self.bus.empty()
         self.assertEqual(0, self.bus.passenger_count())
 
-    @unittest.skip("Delete this line to run the test")
     def test_can_pick_up_passenger_from_bus_stop(self):
+        # Name each person and add age,creating a thing, with the description name and age
         person_1 = Person("Guido van Rossum", 64)
         person_2 = Person("Carol Willing", 50)
+        # Name bus stop
         bus_stop = BusStop("Waverly Station")
+        # adds the list items to the list 'queue' from the file bus_stop
         bus_stop.add_to_queue(person_1)
         bus_stop.add_to_queue(person_2)
+        # calls new method from bus
         self.bus.pick_up_from_stop(bus_stop)
+        # checks if everything is OK
         self.assertEqual(2, self.bus.passenger_count())
